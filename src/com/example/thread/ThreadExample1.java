b0c0cb98efaf97dfcca6721ae4c0b7a38f157448
vimalesan <vimalesan.a@gmail.com>
2015-09-11 00:20:59 +0530

    My Thread Example

--- src/com/example/thread/ThreadExample1.java
+++ src/com/example/thread/ThreadExample1.java
@@ -2,7 +2,18 @@
 package com.example.thread;
 
 public class ThreadExample1 {
+	private static class MyThread implements Runnable{
+
+		@Override
+		public void run() {
+			System.out.println("Runnable Way ::::: "+Thread.currentThread().getName());
+			
+		}
+		
+	}
 	public static void main(String args[]){
-		System.out.println("Hello World");
+		Thread t1 = new Thread(new MyThread(),"New Thread");
+		t1.start();
+		System.out.println("Main Thread ::::: "+Thread.currentThread().getName());
 	}
 }
